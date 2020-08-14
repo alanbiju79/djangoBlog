baf16bd381eab976053dc093eef5cc776b9ec41b
Alan Biju <alanbiju79@gmail.com>
2020-08-14 01:23:50 +0100

    Added login and logout functionality

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
+@login_required
 def about(request):
     return render(request, 'blog/about.html')
